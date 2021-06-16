ef3d6a8fb97510cd84afc641e5b9a757e0ac6926
AmishaSoni <amishasoni2001@gmail.com>
2021-06-16 13:22:23 +0530

    Initial Project

---
+++ src/Error.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Error.css'
+
+const Error = () => {
+    return (
+        <>
+        <div className="error">
+            <h1>404 Error Page.. </h1>
+            <p> Sorry , This page doesn't exist !!! </p> 
+            <button className="btn">Go Back</button>
+        </div>
+        </>
+    )
+}
+
+export default Error;
